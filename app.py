b02f26595a98884cc7e3719cb0429d0d6a4054c5
mizuki3375 <mizuki.miyakawa3375@gmail.com>
2024-07-01 16:50:03 +0900

    fix

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import yfinance as yf
 
+
 st.write("""
 # My first app
 Hello *world!*
 """)
-df = yf.download("AAPL", start="2010-11-01", end="2020-11-01")
+df = yf.download("AAPL", start="2010-11-01", end="2020-11-01", progress=False)
 st.line_chart(df)
